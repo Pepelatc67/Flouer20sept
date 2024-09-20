589d6f083e0f43b228f146f8d8f689312580e033
dashkov.v.e <dmitrieva.kg@gmail.com>
2024-09-20 12:57:11 +0300

    Вввел переменные

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         String V,C,G;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("введите число дней:");
         int day = scanner.nextInt();
