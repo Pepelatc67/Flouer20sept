2fbe204933367c7620510a325703111384a3a207
arhimarhi <dmitrieva.kg@gmail.com>
2024-09-20 11:57:32 +0300

    ddd

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+int day;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число дней:");
+        int day = scanner.nextInt();
+    }
+}
